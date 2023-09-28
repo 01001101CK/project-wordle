ba1652e60e45953ca0420d557cc7f507294b5a2e
Yufa <112290188+01001101CK@users.noreply.github.com>
2023-09-28 16:48:40 -0700

    replace min and max and required with pattern and tile for better user experience

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -23,6 +23,14 @@ function Input() {
         value={guess}
         minLength={5}
         maxLength={5}
+          //The pattern attribute uses Regular Expression syntax. 
+          //Specifically what we're saying here is that the acceptable characters 
+          //are letters ([a-zA-Z]), and that there should be exactly 5 of them {5}.
+          //We also add the title attribute for clarification; 
+          //if the user tries to submit the form and the validation isn't met, 
+          //they'll be shown a message like this:
+        pattern="[a-zA-Z]{5}"
+        title="5 letter word"
         onChange={event => {
           const upperCase = event.target.value.toUpperCase();
           setGuess(upperCase);

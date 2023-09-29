31e3c770f99ad44ee883342f8c85046ecd7cbed3
Yufa <112290188+01001101CK@users.noreply.github.com>
2023-09-28 20:12:37 -0700

    add some CSS classes to color the background of each cell, based on the results and the correct answer

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { range } from '../../utils'
+import { checkGuess } from '../../game-helpers'
 
-function Guess({value}) {
-  
+function Guess({ value, answer }) {
+  const checkResult = checkGuess(value, answer);
+
+
   return (
       <p className="guess">
     {range(5).map( number => (
-          <span key={crypto.randomUUID()} className="cell">{value ? value[number] : undefined}</span>
+      <span key={crypto.randomUUID()} className={value ? `cell ${checkResult[number].status}` : 'cell'}>{value ? value[number] : undefined}</span>
       ))}
       </p>
     );
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -3,13 +3,13 @@ import Guess from '../Guess'
 import {range} from '../../utils'
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants'
 
-function RenderInput({guesses}) {
+function RenderInput({ guesses, answer }) {
   
 
   return (
     <div className="guess-results">
       {range(NUM_OF_GUESSES_ALLOWED).map( number => (
-     <Guess key={crypto.randomUUID()} value={guesses[number]} />
+        <Guess key={crypto.randomUUID()} value={guesses[number]} answer={answer}/>
   ))}
     </div>
   )
